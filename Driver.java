817ce9db7489f14a8b0582d5b8f1f6b0adad55fb
Austin Back <tartsofficial@gmail.com>
2021-06-10 21:18:18 -0400

    Initial commit

---
+++ Driver.java
@@ -0,0 +1,14 @@
+public class Driver extends ListenerAdapter{
+
+public static void main(String[] args){
+        //these two lines create a jda instance and launch it
+        JDAMaker jda = new JDAMaker("your token here","bots playing status here");
+        jda.constructJDA();
+    }
+    //this an example of a listener for an event, see jda documentation for all events
+    @Override
+    public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+        //this is a class that holds instructions for when bot recieves a message
+        new MessageRecieved(event);
+    }//ends message received event    
+}
