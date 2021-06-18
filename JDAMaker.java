635567d5ca7fe532ba2722eb5c761f46a9f2248a
Austin Back <tartsofficial@gmail.com>
2021-06-17 21:45:16 -0400

    added a music bot jda constructor method

--- JDAMaker.java
+++ JDAMaker.java
@@ -1,3 +1,13 @@
+package Music.Testing.Bot;
+
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
+
 public class JDAMaker {
 	
 	String token, setGame;
@@ -7,25 +17,38 @@ public class JDAMaker {
 		this.setGame = setGame;
 	}
 	
-	public void constructJDA(){
-		try
-        {
-            JDA jda = JDABuilder.createLight(token)// The token of the account that is logging in.
-            		.addEventListeners(new Driver())// An instance of a class that will handle events.
-            		
-                    .build();
-            jda.getPresence().setActivity(Activity.playing(setGame));
-            jda.awaitReady(); // Blocking guarantees that JDA will be completely loaded.
-            System.out.println("Finished Building JDA!");
-        }
-        catch (LoginException e)
-        {
-            e.printStackTrace();
-        }
-        catch (InterruptedException e)
-        {
-            e.printStackTrace();
-        }
+	public void constructJda(){
+		try {
+            JDA jda = JDABuilder.createLight(token)
+            			.addEventListeners(new Driver())// An instance of a class that will handle events.
+            			.build();
+			jda.getPresence().setActivity(Activity.playing(setGame));
+           		jda.awaitReady(); // Blocking guarantees that JDA will be completely loaded.
+            		System.out.println("Finished Building JDA!");
+        	}catch (LoginException e) {
+           		 e.printStackTrace();
+        	}catch (InterruptedException e) {
+            		e.printStackTrace();
+       		}
+	}
+	
+	public void constructJdaWithMusic(){
+		try {
+           		JDA jda = JDABuilder.createDefault(token,
+            			GatewayIntent.GUILD_MEMBERS,
+            			GatewayIntent.GUILD_MESSAGES,
+            			GatewayIntent.GUILD_VOICE_STATES/*this is only needed for music bots*/)
+            			.addEventListeners(new Driver())// An instance of a class that will handle events.
+            			.enableCache(CacheFlag.VOICE_STATE)//only need this for music bots
+                   	 	.build();
+            		jda.getPresence().setActivity(Activity.playing(setGame));
+           		jda.awaitReady(); // Blocking guarantees that JDA will be completely loaded.
+            		System.out.println("Finished Building JDA!");
+        	} catch (LoginException e) {
+           		 e.printStackTrace();
+        	} catch (InterruptedException e) {
+            		e.printStackTrace();
+       		}
 	}
 
 }
